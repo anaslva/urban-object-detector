d2ebd14bf349b3cea131a8df50c1420a0630c58e
anaslva <anacarolina.slva@hotmail.com>
2025-05-16 16:14:32 -0300

    add object counter and realtime detection

---
+++ src/object_counter.py
@@ -0,0 +1,63 @@
+import cv2
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from ultralytics import YOLO
+from tqdm import tqdm
+
+video_path = "src/videos/39031.avi"  
+frame_limit = 100  
+os.makedirs("src/frames", exist_ok=True)
+
+cap = cv2.VideoCapture(video_path)
+success, frame = cap.read()
+frame_count = 0
+
+print("....extraindo frames do vídeo....")
+
+while success and frame_count < frame_limit:
+    frame_file = f"src/frames/frame_{frame_count:04d}.jpg"
+    cv2.imwrite(frame_file, frame)
+    success, frame = cap.read()
+    frame_count += 1
+    
+print(f"!...{frame_count} frames salvos...!")
+
+#modelo 
+
+model = YOLO("yolov5su.pt")
+
+total_count = {}
+
+def update_count(detected_labels):
+    for label in detected_labels:
+        total_count[label] = total_count.get(label, 0) + 1
+        
+
+print("...processando frames com YOLO...")
+
+for i in tqdm(range(frame_count)):
+    frame_path = f"src/frames/frame_{i:04d}.jpg"
+    results = model(frame_path, verbose=False)[0]
+    labels = [model.names[int(cls)] for cls in results.boxes.cls.tolist()]
+    update_count(labels)
+
+df = pd.DataFrame(total_count.items(), columns=["Class", "Quantity"])
+urban_classes = ["car", "truck", "bus", "motorbike", "bicycle", "person"]
+df = df[df["Class"].isin(urban_classes)]
+df.sort_values("Quantity", ascending=False, inplace=True)
+df.to_csv("final_count.csv", index=False)
+
+print("\ncontagem final por classe:")
+print(df)
+
+# gráfico
+
+plt.figure(figsize=(8, 5))
+plt.bar(df["Class"], df["Quantity"], color="skyblue")
+plt.title("Detected objects count (YOLOv5 - UA-DETRAC)")
+plt.ylabel("Quantity")
+plt.xlabel("Class")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
---
+++ src/real_time_detection.py
@@ -0,0 +1,44 @@
+import cv2
+from ultralytics import YOLO
+
+video_path = "src/videos/39031.avi"
+
+model = YOLO("yolov5s.pt")
+
+cap = cv2.VideoCapture(video_path)
+if not cap.isOpened():
+    print("erro")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, verbose=False)[0]
+
+    # Desenhar boxes e labels no frame
+    for box in results.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        cls_id = int(box.cls[0])
+        label = model.names[cls_id]
+        conf = float(box.conf[0])
+
+        if conf < 0.3:
+            continue  # Ignora detecções fracas
+
+        # Desenhar retângulo e label
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        text = f"{label} {conf:.2f}"
+        cv2.putText(frame, text, (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    cv2.imshow("Detecção - YOLOv5", frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
